10c1b4abac7b08c34ab576e95bbad9f8dbccf0b4
locpeople <info@locpeople.com>
2017-07-03 12:28:23 +0100

    Update prov.sh

--- prov.sh
+++ prov.sh
@@ -34,8 +34,6 @@ cd vim
 ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp=yes \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config \
             --enable-python3interp=yes \
             --with-python3-config-dir=/usr/lib/python3.5/config \
             --enable-perlinterp=yes \
