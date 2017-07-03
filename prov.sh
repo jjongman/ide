7dd5e94e793e34d0ba62752fb5378c404a1a9d8b
locpeople <info@locpeople.com>
2017-07-03 12:04:35 +0100

    Update prov.sh

--- prov.sh
+++ prov.sh
@@ -15,7 +15,7 @@ apt-get update
 apt-get dist-upgrade -y
 apt-get install -y sudo
 apt-get install -y git
-apt-get install gcc-5 g++-5
+apt-get install -y gcc-5 g++-5
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
 useradd $username
